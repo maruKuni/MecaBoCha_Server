159fda556c5595a56407e2b89f68538e5853e35a
國丸裕太 <yu.kuni.maru@gmail.com>
2022-01-19 19:30:22 +0900

    add include

--- myUtil.h
+++ myUtil.h
@@ -1,6 +1,13 @@
 #ifndef MY_UTIL_H
 #define MY_UTIL_H
+#include <stdio.h>
+#include <stdlib.h>
 #include <arpa/inet.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
 typedef struct CLIENT{
     struct sockaddr_in info;
     int sockfd;
